a55c782b5405fee0e8e7536a76c8c0bf4a848bc1
facuvulcano <facuvulcano1999@gmail.com>
2024-09-10 11:44:53 -0300

    TP2 terminado y inicio de TP3

---
+++ Vulcano_Facundo_TP3/models.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+class Node():
+    def __init__(self, data, feature_idx, feature_val, prediction_probs, information_gain) -> None:
+        self.data = data
+        self.feature_idx = feature_idx
+        self.feature_val = feature_val
+        self.prediction_probs = prediction_probs
+        self.information_gain = information_gain
+        self.left = None
+        self.right = None
+
+
+class DecisionTree():
+
+    def __init__(self,
+                 max_depth,
+                 min_samples_leaf,
+                 min_information_gain):
+        pass
+
+    def entropy(self, class_probabilties: list) -> list:
+        pass
+
+    def class_probabilites(self, labels: list) -> list:
+        pass
+
+    def datta_entropy(self, labels: list) -> float:
+        pass
+
+    def partition_entropy(self, suvsets:list) ->  float:
+        pass
+
+    def split(self, data: np.array, feature_idx: int, feature_val: float) -> tuple:
+        pass
+
+    def find_best_split(self, data: np.array) -> tuple:
+        pass
+
+    def find_label_probs(self, data: np.array) -> np.array:
+        pass
+    
+    def create_tree(self, data: np.array, current_depth: int) -> Node:
+        pass
+
+    def predict_one_sample(self, X: np.array) -> np.array:
+        pass
+
+
+
